a1b0ff67a3780851f93d53289707dc6e382d7c5f
Vadim Fridman <vadim.fridman@btelligent.com>
2023-12-18 01:51:35 +0100

    Initial commit

---
+++ models/staging/stg_tpch_part_suppliers.sql
@@ -0,0 +1,14 @@
+
+{{
+    config(
+        materialized = 'ephemeral'
+    )
+}}
+select
+    partkey as part_key,
+	suppkey as supplier_key,
+    availqty as supplier_availabe_quantity,
+    supplycost as supplier_cost_amount,
+    comment as part_supplier_comment
+from
+    {{ source('tpch_raw', 'partsupp') }}
---
+++ models/staging/stg_tpch_region.sql
@@ -0,0 +1,11 @@
+{{
+    config(
+        materialized = 'ephemeral'
+    )
+}}
+select
+    regionkey as region_key,
+    name as region_name,
+    comment as region_comment
+from
+    {{ source('tpch_raw', 'region') }}
