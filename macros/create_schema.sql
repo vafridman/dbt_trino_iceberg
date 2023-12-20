ead5361bfdb0f0e16b7a9ba7a42a19e1ba697cc6
Vadim Fridman <vadim.fridman@btelligent.com>
2023-12-20 20:08:14 +0100

    orders_snapshot added + minor changes

--- macros/create_schema.sql
+++
@@ -1,5 +0,0 @@
-{% macro trino__create_schema(relation) -%}
-{%- call statement('create_schema') -%}
-    CREATE SCHEMA {{ relation.schema }} WITH (location='s3://lakehouse/')
-{% endcall %}
-{% endmacro %}
